db3a08dc2f1c024f7cbc740cb61d7d20549de99a
Isaac Smith <isaac@isrv.pw>
2015-01-13 21:25:01 -0600

    Did things to thing.

--- web/lazytag.js
+++ web/lazytag.js
@@ -1,3 +1,10 @@
+var player_cols = [
+    'name',
+    'connected',
+    'power',
+    'id'
+]
+
 function handlemessage(event) {
     data = JSON.parse(event.data);
     
@@ -10,10 +17,32 @@ function handlemessage(event) {
         }
     }
     
-    else if (data.player !== undefined) {}
+    else if (data.player !== undefined) {
         // remember - ordering player list: document.getElementById('body').insertBefore(ele1, ele2);
+        var row = document.getElementById(data.player.id);
+        if (row === null) {
+            row = document.createElement('tr');
+            row.setAttribute('id', data.player.id);
+            
+            for (col in player_cols) {
+                td = document.createElement('td');
+                row.appendChild(td);
+            }
+            
+            document.getElementById('playerlistbody').appendChild(row);
+        }
         
-        
+        for (col in player_cols) {
+            if (data.player[player_cols[col]] !== undefined) {
+                if (player_cols[col] == 'connected') {
+                     if (data.player.connected) {data.player.connected = 'Connected';}
+                     else {data.player.connected = 'Disconnected';}
+                }
+                
+                row.getElementsByTagName('td')[col].innerHTML = data.player[player_cols[col]];
+            }
+        }
+    }
 }
 
 function sendevent(event) {
@@ -23,7 +52,7 @@ function sendevent(event) {
 var gamesettings_state = false
 function togglesettings() {
     if (!gamesettings_state) {
-        document.getElementById('gamesettings').classList.add('expand');
+        document.getElementById('gamesettings').classList.add('expand');            
         gamesettings_state = true;
     }
     
